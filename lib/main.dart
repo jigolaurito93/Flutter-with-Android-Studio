9c139ab1e3db7e9209aae7603da3b1fb1d848ff3
Jose Laurito <jigolaurito93@yahoo.com>
2024-04-08 03:27:03 -0500

    refactor

--- lib/main.dart
+++ lib/main.dart
@@ -3,16 +3,18 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-        home: Scaffold(
-            backgroundColor: Colors.blueGrey,
-            appBar: AppBar(
-              title: const Text('I am Rich Boys'),
-              backgroundColor: Colors.blueGrey[900],
-            ),
-            body: const Center(
-              child: Image(
-                image: AssetImage('images/diamond.png'),
-              ),
-            ))),
+      home: Scaffold(
+        backgroundColor: Colors.blueGrey,
+        appBar: AppBar(
+          title: const Text('I am Rich Boys'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: AssetImage('images/diamond.png'),
+          ),
+        ),
+      ),
+    ),
   );
 }
